4e54b1f84bd2b6b96a2709642cdc920f87cc8e53
Ashikawa <ashikawaharuki@MUMac.local>
2024-08-14 16:45:04 +0900

    仕様の一新

--- app.py
+++ app.py
@@ -1,82 +1,23 @@
 from dash import Dash, dcc, html, Input, Output, callback
+from pathlib import Path
+import pandas as pd
+import plotly.express as px
 
 
-app = Dash(__name__)
+# 現在のスクリプトが置かれているディレクトリを取得
+PROJ_DIR = Path(__file__).resolve().parents[0]
+DATA_DIR = PROJ_DIR/'data'
+
+app = Dash(__name__, suppress_callback_exceptions=True)
 
 
 app.layout = html.Div([
     dcc.RadioItems(
         options=['English', 'Japanese'],
-        value='English',
+        value='Japanese',
         id='language-selector',
     ),
-    html.Div(id='main-content')
 ])
-
-
-# 以下にメインコンテンツが続く。
-
-EN_LAYOUT = [
-    html.Div([
-        html.Div([
-            html.H5('Select how you want to find clusters'),
-            dcc.RadioItems(
-                # TODO:option名をいい感じにする
-                    options=['Find clusters from visualisation', 'Use the clusters prepared here'],
-                    value='Find clusters from visualisation',
-                    id='cluster_type1',
-                    inline=True,
-                ),
-        ], className="box"),
-        html.Div([
-            html.H5('Select how you want to find clusters'),
-            dcc.RadioItems(
-                # TODO:option名をいい感じにする
-                    options=['Find clusters from visualisation', 'Use the clusters prepared here'],
-                    value='Find clusters from visualisation',
-                    id='cluster_type',
-                    inline=True,
-                ),
-        ], className="box"),
-    ], className="container"),
-]
-
-
-JA_LAYOUT = [
-    html.Div([
-        html.Div([
-            html.H5('クラスタの見つけ方を選択'),
-            dcc.RadioItems(
-                # TODO:option名をいい感じにする
-                    options=['可視化からクラスタを見つける', '用意したクラスタを使用する'],
-                    value='可視化からクラスタを見つける',
-                    id='cluster_type1',
-                    inline=True,
-                ),
-        ], className="box"),
-        html.Div([
-            html.H5('クラスタの見つけ方を選択'),
-            dcc.RadioItems(
-                # TODO:option名をいい感じにする
-                    options=['可視化からクラスタを見つける', '用意したクラスタを使用する'],
-                    value='可視化からクラスタを見つける',
-                    id='cluster_type',
-                    inline=True,
-                ),
-        ], className="box"),
-    ], className="container"),
-]
-
-
-REPORT_D = {"English":EN_LAYOUT, "Japanese":JA_LAYOUT}
-
-
-@callback(
-        Output('main-content', 'children'),
-        Input('language-selector', 'value')
-)
-def select_language(language):
-    return REPORT_D[language]
 
 
 if(__name__ == '__main__'):
--- start.sh
+++
@@ -1,9 +0,0 @@
-# app.pyを実行する過程を自動化
-
-PORT = 8050
-PID = $(lsof -t -i:$PORT)
-if [ ! -z "$PID" ]; then
-    kill -9 $PID
-    echo "Killed process $PID on port $PORT"
-fi
-python3 ./app.py
