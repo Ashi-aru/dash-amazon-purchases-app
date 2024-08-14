1dfb2b20c258b00954be958541f610d801f776fb
Ashikawa <ashikawaharuki@MUMac.local>
2024-08-14 16:38:57 +0900

    仕様の一新によりデータの前処理プロセスも一新

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -8,34 +8,24 @@ DATA_DIR = PROJ_DIR/'data'
 
 def generate_new_tabular_data(new_index='Survey ResponseID'):
     """
-    new_indexで指定された値をインデックスにもつ派生表データを生成する
-    Generate derivied tabular data which have the value specified in new_index as index.
+    数的データである、注文額、注文量、注文回数を以下の階層性に分けて整理した表データを生成。
+    - ユーザーID
+        - 注文時期（Y-M）
+            - カテゴリ
 
-    new_indexは "Survey ResponseID" または "ASIN/ISBN (Product Code)"
-    new_index is "Survey ResponseID" or "ASIN/ISBN (Product Code)"
-
-    cullumnは、
-    - "user"の場合: ユーザーID(Survey ResponseID), 住所, 購入額（合計, 各年, 各年）, 購入数（合計, 各年, 各年）
-    - "product"の場合: 商品ID(ASIN/ISBN), カテゴリ, 値段（平均）, 販売額（合計, 各年, 各年）, 販売数（合計, 各年, 各年）
-    Cullumns are,
-    - "user": user ID(Survey ResponseID), address, value of purchases (total, each year, each year), number of purchases (total, each year, each year)
-    - "product": product ID(ASIN/ISBN), category, price (average), amount sold (total, each year, each year), number sold (total, each year, each year)
     """
     df = add_derived_attriubte(df = pd.read_csv(DATA_DIR/'amazon-purchases.csv'))
 
-    df_categories = generate_new_categorical_tabular_data(df=df, new_index=new_index)
-    df_Y = generate_new_quantitative_tabular_data(df=df, new_index=new_index, time_span='Y')
-    df_M = generate_new_quantitative_tabular_data(df=df, new_index=new_index, time_span='M')
-
-    new_df = df_categories.join(df_Y, on=new_index).join(df_M, on=new_index)
-    file_name = 'userID' if(new_index=='Survey ResponseID') else 'productID'
-    new_df.to_csv(DATA_DIR/f'derived_{file_name}.csv', index=True)
+    df_categories = generate_new_categorical_tabular_data(df=df)
+    df_quantitative = generate_new_quantitative_tabular_data(df=df)
+    new_df =  df_quantitative.join(df_categories, on=new_index)
+    return new_df.to_csv(DATA_DIR/'target.csv', index=True)
 
 
 def add_derived_attriubte(df):
     df['Order Date'] = pd.to_datetime(df['Order Date'])
-    df['Order Year'] = df['Order Date'].dt.to_period('Y')
-    df['Order Month'] = df['Order Date'].dt.to_period('M')
+    df['Order Year'] = df['Order Date'].dt.year
+    df['Order Month'] = df['Order Date'].dt.month
     df['total price'] = df['Purchase Price Per Unit']*df['Quantity']
     return df
 
@@ -45,25 +35,12 @@ def generate_new_categorical_tabular_data(df, new_index='Survey ResponseID'):
     return df.groupby(new_index).agg(comb_d)
 
 
-def generate_new_quantitative_tabular_data(df, new_index='Survey ResponseID', time_span='Y'):
+def generate_new_quantitative_tabular_data(df, new_index='Survey ResponseID'):
     comb_d = {'total price':'sum', 'Quantity':'sum'} if(new_index=='Survey ResponseID') else {'total price':'sum', 'Quantity':'sum', 'Purchase Price Per Unit':'mean'}
-    time_scale = 'Order Year' if(time_span=='Y') else 'Order Month'
-    group= df.groupby([new_index, time_scale]).agg(comb_d)
-
-    # new_indexはIndexに固定したまま、それ以外を転置する
-    # new_index remains fixed as Index and transposes the others
-    df_ = group.unstack(level=new_index).T
-    # 転置したdfを二つ目のindexによって分離
-    # Separate the transposed df by a second index
-    df_p, df_q = df_.xs('total price',level=0), df_.xs('Quantity',level=0)
-    # カラム名に"価格を表すP" or "量を表すQ"のいずれかを適宜付け加える
-    # Add either "P for price" or "Q for quantity" to the column name appropriately.
-    df_p.columns = ['P_'+str(c) for c in df_p.columns]
-    df_q.columns = ['Q_'+str(c) for c in df_q.columns]
-    # new_indexを軸に、横向きに合体させる
-    return df_p.join(df_q, on=new_index)
-
+    multi_index = [new_index, 'Order Year', 'Order Month', 'Category']
+    group1= df.groupby(multi_index).agg(comb_d)
+    group2= df.groupby(multi_index).agg(Count=('Survey ResponseID', 'count'))
+    return group1.join(group2, on=multi_index)
 
 if(__name__=='__main__'):
     generate_new_tabular_data(new_index='Survey ResponseID')
-    generate_new_tabular_data(new_index='ASIN/ISBN (Product Code)')
