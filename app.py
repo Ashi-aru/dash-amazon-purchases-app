4963aed5be715022da08011cbaa44be0636a84b1
Ashikawa <ashikawaharuki@MUMac.local>
2024-08-16 16:33:42 +0900

    グラフの設定について実装

--- app.py
+++ app.py
@@ -16,8 +16,87 @@ app.layout = html.Div([
         options=['English', 'Japanese'],
         value='Japanese',
         id='language-selector',
+        inline=True,
     ),
+    html.Div([
+        # クラスタ探索方法の設定（散布図 or Parallel Cordinates Plot）
+        dcc.Dropdown(
+            options=[
+                {'label': 'Scatter Plot', 'value':'Scatter Plot'},
+                {'label': 'Parallel Cordinates Plot', 'value':'Parallel Cordinates Plot'},
+            ],
+            placeholder='クラスタ探索方法を選択',
+            id='graph_type',
+            className='mbox',
+        ),
+
+        # グラフの設定
+        html.Div(id='plot_config', className='mbox container'),
+
+        # グラフの出力
+        dcc.Graph(id='main_plot'),
+    ], className='bbox')
 ])
+
+
+@callback(
+    Output('plot_config', 'children'),
+    Input('graph_type', 'value'),
+)
+def update_config(graph_type):
+    if(graph_type=='Scatter Plot'):
+        children = [
+            html.Div([
+                dcc.Dropdown(
+                    options=[
+                        {'label': 'OOO', 'value':'OOO'},
+                        {'label': 'xxx', 'value':'xxx'},
+                    ],
+                    placeholder='x軸の値を選択',
+                    id='x_axis_selector',
+                ),
+                dcc.RadioItems(
+                    options=['linear', 'log'],
+                    value='linear',
+                    id='x_axis_config',
+                    inline=True
+                ),
+            ], className='sbox'),
+            html.Div([
+                dcc.Dropdown(
+                    options=[
+                        {'label': 'OOO', 'value':'OOO'},
+                        {'label': 'xxx', 'value':'xxx'},
+                    ],
+                    placeholder='y軸の値を選択',
+                    id='y_axis_selector',
+                ),
+                dcc.RadioItems(
+                    options=['linear', 'log'],
+                    value='linear',
+                    id='y_axis_config',
+                    inline=True
+                ),
+            ], className='sbox'),
+            # できることならここにクエリを入れる
+        ]
+    if(graph_type=='Parallel Cordinates Plot'):
+        children = [
+            dcc.Dropdown(
+                options=[
+                    {'label': 'OOO', 'value':'OOO'},
+                    {'label': 'xxx', 'value':'xxx'},
+                ],
+                placeholder='軸の値を選択',
+                id='parallel_axis_config',
+                multi=True,
+                className='custom-dropdown',
+            ),
+            # ここに各ラベルに対してlinear or logを選択するボタンを入れたい
+        ]
+    return children
+
+
 
 
 if(__name__ == '__main__'):
